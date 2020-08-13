0f04cd6a3c04d532454573430264a7a965c7edba
stevezhee <brettletner@gmail.com>
2020-08-13 09:06:54 -0500

    cp

--- Doc.c
+++ Doc.c
@@ -52,8 +52,6 @@ void docGitCommit(doc_t *doc)
 {
   if (DEMO_MODE || NO_GIT) return;
   docGitAdd(doc);
-  sprintf(systemBuf, "git commit -m\"cp\" %s", doc->filepath);
-  system(systemBuf);
 }
 
 void docMake(doc_t *doc)
