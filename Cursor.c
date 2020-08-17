3a43401aca76c04e80a804adb606453a799921f0
stevezhee <brettletner@gmail.com>
2020-08-17 10:10:52 -0500

    cp

--- Cursor.c
+++ Cursor.c
@@ -1,13 +1,13 @@
-""//
+//
 //  Cursor.c
 //  editordebugger
 //
 //  Created by Brett Letner on 5/27/20.
 //  Copyright © 2020 Brett Letner. All rights reserved.
 //
-""
+
 #include "Cursor.h"
-''
+
 void cursorCopy(cursor_t *dst, cursor_t *src) {
   memcpy(dst, src, sizeof(cursor_t));
 }
