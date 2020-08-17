4d77e03cfd94d57e687e9c75fe1895d6e9a8c718
stevezhee <brettletner@gmail.com>
2020-08-17 09:23:44 -0500

    cp

--- main.c
+++ main.c
@@ -2001,8 +2001,9 @@ CORE:
     Goto line
     insert both open/close characters (e.g. "" or {}).  If selection is on, use
 that for contents.
- Scroll when mouse selection goes off screen command line
-args/config to set config items (e.g. demo mode) status bar that has modified,
+    Scroll when mouse selection goes off screen command line
+    args/config to set config items (e.g. demo mode)
+status bar that has modified,
 etc. cut/copy/paste with mouse create new file input screen (for search, paste,
 load, tab completion, etc.) Hook Cut/Copy/Paste into system Cut/Copy/Paste
     support editor API
