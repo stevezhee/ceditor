9c4855e5807b4fac59fb4a26558a6e81547f9627
stevezhee <brettletner@gmail.com>
2020-08-19 10:37:21 -0500

    cp

--- main.c
+++ main.c
@@ -1582,20 +1582,6 @@ void pasteBefore() {
     insertCString(s);
 }
 
-void copy(char *s, uint len) {
-  if (focusViewRef() == COPY_BUF) {
-    copyElemToClipboard();
-    return;
-  }
-
-  int refFrame = focusFrameRef();
-  setFocusBuiltinsView(COPY_BUF);
-  insertNewElem();
-  builtinInsertString(s, len);
-  setClipboardText(focusDoc()->contents.start);
-  setFocusFrame(refFrame);
-}
-
 void docPushCommand(commandTag_t tag, doc_t *doc, int offset, char *s,
                     int len) {
   command_t *cmd;
