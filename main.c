478df1ae0f7310c330ef95543b182f828697f5f4
stevezhee <brettletner@gmail.com>
2020-08-19 09:40:40 -0500

    cp

--- main.c
+++ main.c
@@ -1167,7 +1167,7 @@ void focusScrollY(int dR) {
 
 void focusTrackRow(int row)
 {
-  printf("row = %d\n");
+  printf("row = %d\n", row);
   int height = AUTO_SCROLL_HEIGHT;
   view_t *view = focusView();
   int scrollR = view->scrollY / st.font.lineSkip;
