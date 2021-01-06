a2ded1501209fd0508cf0f8b8d286c5727966481
stevezhee <brettletner@gmail.com>
2021-01-06 07:59:44 -0600

    cp

--- main.c
+++ main.c
@@ -155,7 +155,7 @@ void setFocusBuiltinsView(int ref) {
 }
 
 void frameInit(frame_t *frame) {
-  memset(frame, 0, sizeof(frame_t));
+  // memset(frame, 0, sizeof(frame_t));
   // BAL: put back in! frame->color = FRAME_COLOR;
 
   // BAL: put back in! arrayInit(&frame->views, sizeof(view_t));
